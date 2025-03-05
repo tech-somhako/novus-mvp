d5b398a930f7afb34f6d46554890245c7ab242e7
rahberabbas <amaanabidi1234@gmail.com>
2025-03-05 15:44:25 +0530

    change language to japanese

--- app/components/chat/chat.jsx
+++ app/components/chat/chat.jsx
@@ -384,7 +384,7 @@ export default function ChatView({ messages, onPublish, onHandleSource, isLoadin
             <h5>Brytt</h5>
             <Markdown className="prose prose-lg max-w-none whitespace-pre-line"
               remarkPlugins={[remarkGfm]}
-              rehypePlugins={[rehypeRaw]}>Hello, I am Brytt, How can i assist you today?</Markdown >
+              rehypePlugins={[rehypeRaw]}>こんにちは、私はブレットです。</Markdown >
           </div>
           </div>
       {messages.map((message, index) => (
@@ -448,7 +448,7 @@ export default function ChatView({ messages, onPublish, onHandleSource, isLoadin
                 <path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM11.0026 16L18.0737 8.92893L16.6595 7.51472L11.0026 13.1716L8.17421 10.3431L6.75999 11.7574L11.0026 16Z"></path>
               </svg>
             </div>
-            Publish
+            出版
             {/* </Link> */}
           </Button>
         )}
@@ -467,7 +467,7 @@ export default function ChatView({ messages, onPublish, onHandleSource, isLoadin
                 <path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM11.0026 16L18.0737 8.92893L16.6595 7.51472L11.0026 13.1716L8.17421 10.3431L6.75999 11.7574L11.0026 16Z"></path>
               </svg>
             </div>
-            Start to source profiles
+            プロフィールの調達開始
           </Button>
         )}
 
--- app/components/chat/chatinti.jsx
+++ app/components/chat/chatinti.jsx
@@ -22,7 +22,7 @@ export default function ChatInit() {
           <div>
             <h5>Brytt</h5>
             <p className="text-gray-500">
-              Hey Som, Welcome Back!!! How’s your Day?
+            ソム、おかえりなさい！調子はどうだい？
             </p>
           </div>
         </div>
@@ -44,7 +44,7 @@ export default function ChatInit() {
                     <path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM11.0026 16L18.0737 8.92893L16.6595 7.51472L11.0026 13.1716L8.17421 10.3431L6.75999 11.7574L11.0026 16Z"></path>
                   </svg>
                 </div>
-                Open a New Role?
+                新役割を開く？
               </Link>
             </Button>
 
@@ -61,7 +61,7 @@ export default function ChatInit() {
                   <path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM11.0026 16L18.0737 8.92893L16.6595 7.51472L11.0026 13.1716L8.17421 10.3431L6.75999 11.7574L11.0026 16Z"></path>
                 </svg>
               </div>
-              Yes, Scout similar profiles
+              はい、似たようなプロフィールのスカウト
             </Button>
             <Button
               variant="secondary"
@@ -76,7 +76,7 @@ export default function ChatInit() {
                   <path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 10.5858L9.17157 7.75736L7.75736 9.17157L10.5858 12L7.75736 14.8284L9.17157 16.2426L12 13.4142L14.8284 16.2426L16.2426 14.8284L13.4142 12L16.2426 9.17157L14.8284 7.75736L12 10.5858Z"></path>
                 </svg>
               </div>
-              Calibrate on AI Engineer
+              AIエンジニアのキャリブレーション
             </Button>
           </div>
         </div>
